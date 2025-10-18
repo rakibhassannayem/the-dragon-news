22c8712866547f94e4bac5f54a1c171f1e202f51
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-18 08:38:34 +0600

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { format } from "date-fns";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="flex flex-col gap-3 justify-center items-center my-8">
+      <img src={logo} alt="" />
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+      <p className="font-semibold text-accent">{format(new Date(), "EEEE, MMMM MM, yyyy")}</p>
     </div>
   );
 };
