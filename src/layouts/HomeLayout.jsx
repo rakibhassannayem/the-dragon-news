c8a89665bda9190bf3fa076e217f099b1d458260
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-18 08:55:39 +0600

    marquee headline

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
 
 const HomeLayout = () => {
   return (
     <div>
       <header>
         <Header />
+        <section className="container mx-auto mb-3">
+          <LatestNews />
+        </section>
       </header>
       <main>
         <section className="left-nav"></section>
